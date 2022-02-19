d6394e300d725a8586d94bdf78d8ed08a5d4abbd
Naveen <naveen352kumar@gmail.com>
2022-02-19 22:06:00 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/quizitup/classroom/AddClassroomActivity.java
@@ -0,0 +1,16 @@
+package com.example.quizitup.classroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quizitup.R;
+
+public class AddClassroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_classroom);
+    }
+}
